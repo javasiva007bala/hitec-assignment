13bef9e96b9881977b153b88ba04c9205fbc5059
javasiva007bala <32795225+javasiva007bala@users.noreply.github.com>
2019-08-30 21:21:00 -0700

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,24 @@
+////////////////////////////////////////////
+//                                        //
+//   Authore : Siva Pragsam.v             //
+//   Title   : 7-segment Digits           //
+//                                        //
+////////////////////////////////////////////
+
+
+
+#include<htc.h>
+int main()
+{
+	TRISB=0;
+	int a[]={0x80,0xC0,0xE0,0xF0,0xF8,0xFC,0xFE,0xFF};
+	int i;
+	while(1)
+	{
+			for(i=0;i<8;i++)
+			{
+			PORTB=a[i];
+			for(int j=-30000;j<30000;j++);
+			}
+	}	
+}
